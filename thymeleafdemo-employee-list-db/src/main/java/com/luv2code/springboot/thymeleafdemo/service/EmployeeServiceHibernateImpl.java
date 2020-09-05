60f08eeaa05b5b884dcba105e2e57f092c2c1926
Tobechi Nnakwe <38112880+michaelnnakwe@users.noreply.github.com>
2020-09-05 21:42:59 +0100

    Add files via upload

---
+++ thymeleafdemo-employee-list-db/src/main/java/com/luv2code/springboot/thymeleafdemo/service/EmployeeServiceHibernateImpl.java
@@ -0,0 +1,58 @@
+package com.luv2code.springboot.thymeleafdemo.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.luv2code.springboot.thymeleafdemo.dao.EmployeeRepository;
+import com.luv2code.springboot.thymeleafdemo.entity.Employee;
+
+@Service
+public class EmployeeServiceHibernateImpl implements EmployeeService {
+	
+	@Autowired
+	private EmployeeRepository employeeRepository;
+
+	@Override
+	public List<Employee> findAll() {
+		
+		return employeeRepository.findAllByOrderByLastNameAsc();
+	}
+
+	@Override
+	public Employee findById(int theId) {
+		// TODO Auto-generated method stub
+		Optional<Employee> result = employeeRepository.findById(theId);
+		
+		Employee theEmployee = null;
+		if(result.isPresent()) {
+			theEmployee = result.get();
+		}else {
+			throw new RuntimeException("Did not find employee id: "+theId);
+		}
+		return theEmployee;
+	}
+
+	@Override
+	public void save(Employee theEmployee) {
+		// TODO Auto-generated method stub
+		employeeRepository.save(theEmployee);
+	}
+
+	@Override
+	public void deleteById(int theId) {
+		// TODO Auto-generated method stub
+		employeeRepository.deleteById(theId);
+	}
+	
+	@Override
+	public List<Employee> searchBy(String theFirstName, String theLastName) {
+		
+		return employeeRepository.
+				findByFirstNameContainsAndLastNameContainsAllIgnoreCase(
+						theFirstName, theLastName);	
+	}
+
+}
